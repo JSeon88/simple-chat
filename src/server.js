17291a9dcc1331f87079c849b5a4c8d29d3e0d07
sunny <seonyoung.jeon@injeinc.co.kr>
2023-04-16 22:04:07 +0900

    웹소켓 설정 추가

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import Websocket from "ws";
 
 const app = express();
 
@@ -9,9 +11,16 @@ app.set("views", __dirname + "/views");
 // static 설정
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-console.log("hello");
+// app.listen(3000);
 
-app.listen(3000);
+// http 서버 생성. http 서버에 웹소켓 서버를 띄우기 위해 명시적으로 생성시킴
+// 원래는 명시적으로 호출할 필요가 없음.
+const server = http.createServer(app);
+// 웹 소켓 서버 생성
+const wss = new Websocket.Server({ server });
+
+// http 서버와 wss 서버 둘 다 사용 가능
+server.listen(3000);
